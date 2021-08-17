0b244e3b06974076966c5e1db694970023afccba
Volker <79662570+volker-0@users.noreply.github.com>
2021-08-17 22:52:16 +0300

    Added sound to holybot

--- output/source/main.js
+++ output/source/main.js
@@ -53,7 +53,7 @@ function PlaySongVarFile(name, format, vaar) {
 			vaar.push(media);
 		};
 		media.reset();
-		media.setDataSource(__dir__ + "sounds/"+name);
+		media.setDataSource(__dir__ + "sounds/" + name + format);
 		media.prepare();
 		media.start();
 		return media;
@@ -3314,18 +3314,15 @@ Callback.addCallback("ItemUse", function(coords, item){
     if(item.id == ItemID.hollybot){
         Entity.spawn(coords.x,coords.y,coords.z,"ark:holybot");
         Game.message("Призван проклятый механизмм");
+        PlaySongVarFile("Holybot.mp3", HolybotSounds);
     }
 });
 
-Callback.addCallback("tick", function(){
-    if (World.getThreadTime() % 4 == 0){
-        let mobs = Entity.getAll();
-        mobs.forEach(function(item, index, array) {
-            if(Entity.getTypeAddon(item) == "ark:holybot" && !(hollybotSound == 1)){
-                PlaySongVarFile("Holybot.mp3", HolybotSounds)
-                hollybotSound = 1;
-            };
-        });
+Callback.addCallback("EntiyHurt", function(attacker, entity, damageValue, damageType){
+    if(Entity.getTypeAddon(entity) == "ark:holybot"){
+        if(!hollybotSound == 1){
+            PlaySongVarFile("Holybot", ".mp3", HolybotSounds);
+        };
     };
 });
 
@@ -3333,7 +3330,7 @@ Callback.addCallback("EntityDeath", function(entity,attacker,damageType){
     if(Entity.getTypeAddon(entity) == "ark:holybot"){
         var random = Math.random();
         coords = Entity.getPosition(entity);
-        BlockSourse.getDefaultForActor(entity);
+        BlockSourse.getCurrentWorldGenRegion();
         BlockSourse.spawnDroppedItem(coords.x, coords.y, coords.z, ItemID.hollyingot, randomInteger(5, 15));
         if(random<= 0.25){
             BlockSourse.spawnDroppedItem(coords.x, coords.y, coords.z, ItemID.horngodhelmet, 1);
@@ -3344,6 +3341,7 @@ Callback.addCallback("EntityDeath", function(entity,attacker,damageType){
         } else {
             BlockSourse.spawnDroppedItem(coords.x, coords.y, coords.z, ItemID.horngodboots);
         };
+        StopSound(HolybotSounds);
     };
 });
 
--- src/dev/mobs/holybot.js
+++ src/dev/mobs/holybot.js
@@ -1,40 +1,17 @@
-let hollybotSound = null;
-let HolybotSounds = [];
+let HolybotAmbient = new Sound("Holybot.mp3");
+HolybotAmbient.setInPlayer();
+HolybotAmbient.setLooping(true);
+HolybotAmbient.setVolume(1);
 
 Callback.addCallback("ItemUse", function(coords, item){
     coords = coords.relative;
     if(item.id == ItemID.hollybot){
         Entity.spawn(coords.x,coords.y,coords.z,"ark:holybot");
         Game.message("Призван проклятый механизмм");
+        HolybotAmbient.play();
     }
 });
 
-Callback.addCallback("tick", function(){
-    if (World.getThreadTime() % 4 == 0){
-        let mobs = Entity.getAll();
-        mobs.forEach(function(item, index, array) {
-            if(Entity.getTypeAddon(item) == "ark:holybot" && !(hollybotSound == 1)){
-                PlaySongVarFile("Holybot.mp3", HolybotSounds)
-                hollybotSound = 1;
-            };
-        });
-    };
-});
-
-Callback.addCallback("EntityDeath", function(entity,attacker,damageType){
-    if(Entity.getTypeAddon(entity) == "ark:holybot"){
-        var random = Math.random();
-        coords = Entity.getPosition(entity);
-        BlockSourse.getDefaultForActor(entity);
-        BlockSourse.spawnDroppedItem(coords.x, coords.y, coords.z, ItemID.hollyingot, randomInteger(5, 15));
-        if(random<= 0.25){
-            BlockSourse.spawnDroppedItem(coords.x, coords.y, coords.z, ItemID.horngodhelmet, 1);
-        } else if(random <=0.5){
-            BlockSourse.spawnDroppedItem(coords.x. coords.y, coords.z, ItemID.horngodchestplate, 1);
-        } else if(random<=0.75){
-            BlockSourse.spawnDroppedItem(coords.x, coords.y, coords.z, ItemID.horngodleggins, 1);
-        } else {
-            BlockSourse.spawnDroppedItem(coords.x, coords.y, coords.z, ItemID.horngodboots);
-        };
-    };
-});
+Callback.addCallback("EntityDeath", function(entity, attacker, damageType){
+    HolybotAmbient.stop();
+})
