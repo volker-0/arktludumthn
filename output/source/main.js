684e225fd2dfe17e7627dc54891f973b90a93f79
Volker <79662570+volker-0@users.noreply.github.com>
2022-03-15 21:10:56 +0200

    Made check of Altar structure

--- output/source/main.js
+++ output/source/main.js
@@ -3028,17 +3028,17 @@ if (item.id==ItemID.MultiverseSword)
 
 
 // included from: \Altar.js
-const AltarBlocks =[{x: -3,y :0, z :-2},
-                    {x: -3,y :0, z :-1},
-                    {x: -3,y :0, z :0},
-                    {x: -3,y :0, z :1},
-                    {x: -3,y :0, z :2},
-                    {x: -2,y :0, z :-3},
-                    {x: -2,y :0, z :-2},
-                    {x: -2,y :1, z :-2},
-                    {x: -2,y :2, z :-2},
-                    {x: -2,y :3, z :-2},
-                    {x: -2,y :0, z :-1},
+const AltarBlocks =[{x: -3,y :0, z :-2},//+
+                    {x: -3,y :0, z :-1},//+
+                    {x: -3,y :0, z :0},//+
+                    {x: -3,y :0, z :1},//+
+                    {x: -3,y :0, z :2},//+
+                    {x: -2,y :0, z :-3},//+
+                    {x: -2,y :0, z :-2},//+
+                    {x: -2,y :1, z :-2},//-
+                    {x: -2,y :2, z :-2},//
+                    {x: -2,y :3, z :-2},//
+                    {x: -2,y :0, z :-1},//
                     {x: -2,y :0, z :0},
                     {x: -2,y :1, z :0},
                     {x: -2,y :2, z :0},
@@ -3097,18 +3097,23 @@ const AltarBlocks =[{x: -3,y :0, z :-2},
 IDRegistry.genBlockID("creatoraltar");
 Block.createBlock("creatoraltar", [{name: "Алтарь созидания", texture: [["creatoraltar", 0], ["creatoraltar", 0], ["creatoraltar", 0], ["creatoraltar", 0], ["creatoraltar", 0], ["creatoraltar", 0]], inCreative: true}]);
 
-Callback.addCallback("ItemUse", function(coords, block){
-  if(block.id == BlockID.block){
+Callback.addCallback("ItemUse", function(coords){
+  if(World.getBlock(coords.x, coords.y, coords.z).id == BlockID.creatoraltar){
     let Structure;
-    for (let coord of AltarBlocks) {
-      if(World.getBlock(coords.x - coord.x, coords.y - coord.y - 1, coords.z - coord.z) == 112){
+    for (let i = 0; i < AltarBlocks.length; i++) {
+      if(World.getBlock(coords.x + AltarBlocks[i].x, coords.y + AltarBlocks[i].y - 1, coords.z + AltarBlocks[i].z).id == 112){
         Structure = true;
+        Game.message(String(Structure));
       }else{
         Structure = false;
+        Game.message(coords.x + AltarBlocks[i].x);
+        Game.message(coords.y + AltarBlocks[i].y - 1);
+        Game.message(coords.z + AltarBlocks[i].z);
+        Game.message(World.getBlock(coords.x - AltarBlocks[i].x, coords.y - AltarBlocks[i].y - 1, coords.z - AltarBlocks[i].z).id);
         break;
       };
-    }
-    Game.message(Structure);
+    };
+    Game.message(String(Structure));
   };
 });
 
--- src/dev/Altar.js
+++ src/dev/Altar.js
@@ -67,17 +67,21 @@ const AltarBlocks =[{x: -3,y :0, z :-2},
 IDRegistry.genBlockID("creatoraltar");
 Block.createBlock("creatoraltar", [{name: "Алтарь созидания", texture: [["creatoraltar", 0], ["creatoraltar", 0], ["creatoraltar", 0], ["creatoraltar", 0], ["creatoraltar", 0], ["creatoraltar", 0]], inCreative: true}]);
 
-Callback.addCallback("ItemUse", function(coords, block){
-  if(block.id == BlockID.block){
+Callback.addCallback("ItemUse", function(coords){
+  if(World.getBlock(coords.x, coords.y, coords.z).id == BlockID.creatoraltar){
     let Structure;
-    for (let coord of AltarBlocks) {
-      if(World.getBlock(coords.x - coord.x, coords.y - coord.y - 1, coords.z - coord.z) == 112){
+    for (let i = 0; i < AltarBlocks.length; i++) {
+      if(World.getBlock(coords.x + AltarBlocks[i].x, coords.y + AltarBlocks[i].y - 1, coords.z + AltarBlocks[i].z).id == 112){
         Structure = true;
       }else{
         Structure = false;
+        Game.message(coords.x + AltarBlocks[i].x);
+        Game.message(coords.y + AltarBlocks[i].y - 1);
+        Game.message(coords.z + AltarBlocks[i].z);
+        Game.message(World.getBlock(coords.x - AltarBlocks[i].x, coords.y - AltarBlocks[i].y - 1, coords.z - AltarBlocks[i].z).id);
         break;
       };
-    }
-    Game.message(Structure);
+    };
+    Game.message(String(Structure));
   };
 });
