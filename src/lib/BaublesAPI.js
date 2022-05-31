2c0e6ffadd10a5be33a78c2c50d8f72893bde336
Volker <79662570+volker-0@users.noreply.github.com>
2022-05-31 12:12:20 +0300

    Fix BaublesAPI

--- src/lib/BaublesAPI.js
+++ src/lib/BaublesAPI.js
@@ -163,15 +163,13 @@ var Baubles = /** @class */ (function () {
                 var _a, _b;
                 var data = Baubles.data[playerUid];
                 var old = data.cache[name];
-                //Logger.Log(JSON.stringify(old));
-                //Logger.Log(JSON.stringify(data.cache));
-                if (!old && slot.id != 0 || slot.id !== old) {
+                if ((!old && slot.id != 0) || slot.id !== old) {
                     var client = Network.getClientForPlayer(playerUid);
-                    Logger.Log(old==true)
                     if (old) {
-                        (_a = Baubles.getDesc(old.id)) === null || _a === void 0 ? void 0 : _a.onTakeOff(client, data.container, name);
+                        (_a = Baubles.getDesc(old)) === null || _a === void 0 ? void 0 : _a.onTakeOff(client, data.container, name);
+                    }else{
+                        (_b = Baubles.getDesc(slot.id)) === null || _b === void 0 ? void 0 : _b.onEquip(client, data.container, name);
                     }
-                    (_b = Baubles.getDesc(slot.id)) === null || _b === void 0 ? void 0 : _b.onEquip(client, data.container, name);
                     data.cache[name] = slot.id;
                 }
             });
