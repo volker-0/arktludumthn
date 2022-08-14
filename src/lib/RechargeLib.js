c2a6961960f6b1d88335fce2f4726c2770db0c89
Volker <79662570+volker-0@users.noreply.github.com>
2022-08-15 00:40:54 +0700

    щшо

--- src/lib/RechargeLib.js
+++ src/lib/RechargeLib.js
@@ -11,6 +11,50 @@ let RechargeLib = {
   addRechargeble: function(itemid, itemdata, rechargeTime){
     this.Items.push({item:{id:itemid, data:itemdata}, rechargeTime: rechargeTime});
   },
+  StartTimer: function(time,player){
+    Threading.initThread("recharge"+ player, function(){
+      let waitTime = time * 10;
+      let thisRech = RechargeLib.Recharge[player];
+      let thisContainer = thisRech.container;
+      let thisWindow = thisRech.window;
+
+      thisContainer.openAs(thisWindow);
+
+      thisWindow.setContent({
+        location: {
+          x:550,
+          y:225,
+          width:206,
+          height:50
+        },
+        elements: {},
+        drawing: [{type: "bitmap", bitmap: "bar", height: 242, width: 1000}]
+      });
+
+      Logger.Log("0");
+
+      java.lang.Thread.currentThread().sleep(waitTime);
+
+      for(let i = 1; i<5; i++){
+        thisWindow.setContent({
+          location: {
+            x:550,
+            y:225,
+            width:206,
+            height:50
+          },
+          elements: {},
+          drawing: [{type: "bitmap", bitmap: "bar"+i, height: 242, width: 1000}]
+        });
+
+        Logger.Log(i);
+        java.lang.Thread.currentThread().sleep(waitTime);
+      };
+
+      Logger.Log("End");
+      thisContainer.close()
+    });
+  },
 };
 
 Callback.addCallback("ServerPlayerLoaded", function(player){
@@ -53,49 +97,11 @@ Callback.addCallback("PlayerAttack", function(player, victim){
 
   if(ThisPlayerInformation.isRecharging == false){
     ThisPlayerInformation.isRecharging = true;
-    Threading.initThread("recharge"+ player, function(){
-      let thisRech = RechargeLib.Recharge[player];
-      let thisContainer = thisRech.container;
-      let thisWindow = thisRech.window;
-      let waitTime = time * 10;
-
-      thisContainer.openAs(thisWindow);
-
-      thisWindow.setContent({
-        location: {
-          x:550,
-          y:225,
-          width:206,
-          height:50
-        },
-        elements: {},
-        drawing: [{type: "bitmap", bitmap: "bar", height: 242, width: 1000}]
-      });
-
-      java.lang.Thread.currentThread().sleep(waitTime);
-
-      for(let i = 1; i<5; i++){
-        thisWindow.setContent({
-          location: {
-            x:550,
-            y:225,
-            width:206,
-            height:50
-          },
-          elements: {},
-          drawing: [{type: "bitmap", bitmap: "bar"+i, height: 242, width: 1000}]
-        });
-
-        thisContainer.sendChanges();
-
-        java.lang.Thread.currentThread().sleep(waitTime);
-      };
-
-      thisContainer.close()
-    });
+    RechargeLib.StartTimer(time, player);
   }else{
+    RechargeLib.StartTimer(time, player);
     Game.prevent();
   };
 });
 
-RechargeLib.addRechargeble(267, 0, 13);
+RechargeLib.addRechargeble(267, 0, 40/*13*/);
